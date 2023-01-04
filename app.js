c014b91508e181aeac4fde2a36691c2d37e7055d
quotemasterextremo <121985219+quotemasterextremo@users.noreply.github.com>
2023-01-04 18:13:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function randomCaption()
+{
+  var captions = [
+    "Served up fresh",
+    "Steamin' hot...ooh baby!",
+    "The quotiest quotes",
+    "100% REAL American-made quotes",
+    "These quotes are HUGE",
+    "Some quotes for you.",
+    "Endlessly quotable",
+    "An inspiration sensation",
+    "It's the most quoterful time of the year",
+    "Quotes from the quote sphere",
+    "Warm, crispy, filling quotes, right out of the quote oven",
+    "You can be a quoter too!",
+    "Yes, they really said that"
+  ]; 
+  var caption = captions[Math.floor(Math.random() * (captions.length))];
+  document.getElementById("randomCaption").innerHTML = caption; 
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
